fcbe20d0d6611668a5e8687a95e2885f9f6443b2
Vlere Mustafa <vlere.mustafa@digitalschool.tech>
2025-06-19 07:07:06 -0700

    lesson6

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ function load_scripts(){
 
     wp_enqueue_script('dropdown',get_template_directory().'/js/dropdown.js',array(),'1.0',false);
 
-    wp_enqueue_script('bootstrap2',get_template_directory().'/js/boostrap.bundle.min.js',array('jquery'));
+    wp_enqueue_script('bootstrap2',get_template_directory().'/js/bootstrap.bundle.min.js',array('jquery'));
 
 }
 
@@ -47,5 +47,59 @@ function config() {
 
     add_action('after_setup_theme','config',0);
 
+    function wp_devs_sidebars(){
+        register_sidebar(
+            array(
+                'name'=>'Blog Sidebar',
+                'id'=>'sidebar-blog',
+                'description'=>'This is the Blog Sidebar.You can add your widgets here.',
+                'before_widget'=>'<div class="widget-wrapper">',
+                'after_widget'=>'</div>',
+                'before_title'=>'<h4 class="widget-title">',
+                'after_title'=>'</h4>'
+            )
+        );
+
+        register_sidebar(
+            array(
+                'name'=>'Service 1',
+                'id'=>'services-1',
+                'description'=>'First Service Area',
+                'before_widget'=>'<div class="widget-wrapper"',
+                'after_widget'=>'</div>',
+                'before_title'=>'<h4 class="widget-title">',
+                'after_title'=>'</h4>'
+            )
+        );
+
+         register_sidebar(
+            array(
+                'name'=>'Service 2',
+                'id'=>'services-2',
+                'description'=>'Second Service Area',
+                'before_widget'=>'<div class="widget-wrapper"',
+                'after_widget'=>'</div>',
+                'before_title'=>'<h4 class="widget-title">',
+                'after_title'=>'</h4>'
+            )
+        );
+
+         register_sidebar(
+            array(
+                'name'=>'Service 3',
+                'id'=>'services-3',
+                'description'=>'Third Service Area',
+                'before_widget'=>'<div class="widget-wrapper"',
+                'after_widget'=>'</div>',
+                'before_title'=>'<h4 class="widget-title">',
+                'after_title'=>'</h4>'
+            )
+        );
+
+
+
+    }
+
+    add_action('widgets_init','wp_devs_sidebars');
 
 ?>
--- general-template.php
+++ general-template.php
@@ -27,6 +27,7 @@
                 </div>
 
             </div>
+            
         </main>
 
     </div>
