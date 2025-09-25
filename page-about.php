17558aeb404d84ffc501140008a5868e85c66e78
Vlere Mustafa <vlere.mustafa@digitalschool.tech>
2025-09-25 07:01:10 -0700

    lesson13

---
+++ page-about.php
@@ -0,0 +1,52 @@
+<?php get_header();?>
+
+<div id="content" class="site-content">
+    <main id="main" class="site-main">
+        <section class="about-hero">
+            <div class="container">
+                <h1>About Us</h1>
+                <p>Welcome to our tech corner-where innovation meets explanation</p>
+            </div>
+        </section>
+
+        <section class="about-intro">
+            <div class="container">
+                <h2>Who We Are</h2>
+                <p>We are a team of developers,engineers and tech enthusiasts who love to write about the fast-moving world
+                    technology
+                </p>
+            </div>
+        </section>
+
+        <section class="about-mission">
+            <div class="container">
+                <h2>Our Mission</h2>
+                <p>Our goal is simple:to empower curios minds and passionate techies by delivering accurate.usful and accessible content</p>
+            </div>
+        </section>
+
+        <section class="about-what-we-cover">
+            <div class="container">
+                <h2>What We Cover</h2>
+                <ul>
+                    <li>Programming tutorials and tips</li>
+                    <li>Software and app review</li>
+                    <li>AI/ML and data science insights</li>
+                    <li>Web Development,DevOps and cloud computinf</li>
+                    <li>Trends in tech adn industry news</li>
+                </ul>
+            </div>
+
+        </section>
+
+        <section class="about-contact">
+            <div class="container">
+                <h2>Want to Connect?</h2>
+                <p>We love hearing from our readers! Reach out to via our Page.Follow us on social media to stay updated</p>
+            </div>
+        </section>
+</main>
+
+</div>
+
+<?php get_footer();?>
